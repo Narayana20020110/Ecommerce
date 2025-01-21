aa33e97d415c8dc5b4bc5cafe83c33d28acc55a4
yagna kunchala <nreddy1102002@gmail.com>
2025-01-21 12:43:37 +0530

    rectify images

--- DjangoProjects/settings.py
+++ DjangoProjects/settings.py
@@ -131,3 +131,7 @@ LOGOUT_REDIRECT_URL = 'login'
 
 MEDIA_URL ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+if not DEBUG :
+    from whitenoise import WhiteNoise
+    application = WhiteNoise(application,root='/path/to/media',prefix='/media/')
--- DjangoProjects/urls.py
+++ DjangoProjects/urls.py
@@ -18,9 +18,10 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
-from .settings import MEDIA_ROOT,MEDIA_URL
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('Ecommerce.urls'))
 ]
-urlpatterns += static(MEDIA_URL,document_root = MEDIA_ROOT)
+
+if not settings.DEBUG :
+  urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
