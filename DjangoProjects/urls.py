d0fb964d582592f19d9ed9c85e95ad203941f192
yagna kunchala <nreddy1102002@gmail.com>
2025-01-21 12:22:42 +0530

    rectify images

--- DjangoProjects/urls.py
+++ DjangoProjects/urls.py
@@ -18,8 +18,9 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
+from settings import MEDIA_ROOT,MEDIA_URL
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('Ecommerce.urls'))
 ]
-urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+urlpatterns += static(MEDIA_URL,document_root = MEDIA_ROOT)
