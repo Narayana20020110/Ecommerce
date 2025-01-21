7037855ad565bea77bb74baa91a8d0a45aba3f4c
yagna kunchala <nreddy1102002@gmail.com>
2025-01-21 13:02:49 +0530

    rectify images

--- DjangoProjects/settings.py
+++ DjangoProjects/settings.py
@@ -131,9 +131,3 @@ LOGOUT_REDIRECT_URL = 'login'
 
 MEDIA_URL ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-
-if not DEBUG :
-    from whitenoise import WhiteNoise
-    from django.core.wsgi import get_wsgi_application
-    application = get_wsgi_application()
-    application = WhiteNoise(application,root='/path/to/media',prefix='/media/')
